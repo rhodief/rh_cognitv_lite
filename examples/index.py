67a9fc14a9f890b36ef2687b4c83a7eefe8de8bf
rhodief <rhandref@gmail.com>
2026-03-17 22:38:13 +0000

    Enhance execution examples with sequence and parallel handling, including error management

--- examples/index.py
+++ examples/index.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from rh_cognitv_lite.execution_platform import (
     ExecutionPlatform
 )
+from rh_cognitv_lite.execution_platform.errors import TransientError
 from rh_cognitv_lite.execution_platform.event_bus import EventBus
 from rh_cognitv_lite.execution_platform.execution import Execution
 from rh_cognitv_lite.execution_platform.models import RetryConfig
@@ -22,6 +23,7 @@ async def basic_execution():
     await platform(
         Execution(
             name="Nome execução",
+            kind="TIPO_EVENTO",
             description='Essa é uma descrição teste',
             input_data={'value': 23},
             handler=handler
@@ -30,24 +32,52 @@ async def basic_execution():
 
 async def sequence_execution():
     
+    def handler_ok(value: str):
+        print("Hello, Cognitiv!")
+        return {"greeting": f"Hello, {value}!"} if value else {"greeting": "Hello, World!"}
+    def handler_fail(value: str):
+        raise TransientError(message='Ops erro teste')
+    exec_ok = Execution(
+            name="Execuçao Sucesso",
+            kind="TIPO_EVENTO",
+            description='Essa é uma descrição teste',
+            input_data={'value': 23},
+            handler=handler_ok
+        )
+    exec_fail = Execution(
+            name="Execução Falha",
+            kind="TIPO_EVENTO",
+            description='Essa é uma descrição teste',
+            input_data={'value': 23},
+            handler=handler_fail
+        )
+    async with platform.sequence(group_name='grupo teste', retry_config=RetryConfig(max_attempts = 3)) as seq:
+        seq.add(exec_ok)
+        seq.add(exec_fail)
+        print('Result', await seq.run())
+        
+
+async def parallel_execution():
+    
     def handler(value: str):
         print("Hello, Cognitiv!")
         return {"greeting": f"Hello, {value}!"} if value else {"greeting": "Hello, World!"}
     exec = Execution(
             name="Nome execução",
+            kind="TIPO_EVENTO",
             description='Essa é uma descrição teste',
-            input_data=23,
+            input_data={'value': 23},
             handler=handler
         )
-    async with platform.sequence(group_name='grupo teste', retry_config=RetryConfig(max_attempts = 3)) as seq:
-        seq.add(exec)
-        seq.add(exec)
-        print('Result', await seq.run())
+    async with platform.parallel(group_name='grupo teste', retry_config=RetryConfig(max_attempts = 3)) as par:
+        par.add(exec)
+        par.add(exec)
+        print('Result', await par.run())
 
 async def main():
     #await basic_execution()
     await sequence_execution()
-    
+    #await parallel_execution()
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
